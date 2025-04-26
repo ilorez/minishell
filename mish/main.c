2c5b9f3953138679e8694265d9f0325388125d23
ilorez <zobirnajdaoui@gmail.com>
2025-04-26 15:33:38 +0100

    create basic files struct

---
+++ mish/main.c
@@ -0,0 +1,51 @@
+
+#include "./includes/container.h"
+
+void handle_sigint(int sig) {
+    printf("signal: %d\n", sig);
+    // TODO: here it's should stop process it's running 
+    // if not it's should just show new prompt
+    rl_on_new_line();
+    rl_replace_line("", 0);  // Clear input buffer
+    rl_redisplay();
+}
+
+void init_history() {
+    rl_clear_history();
+    add_history("first command");
+}
+
+int main(int ac, char **av, char **env) {
+
+    // setup data and envirnoment varibale
+    // using ft_setup() form ./setup/setup.c
+    // the setup it's free memory and exit auto on error
+    (void) ac, (void)av, (void)env;
+    char *input;
+    
+    signal(SIGINT, handle_sigint);
+    init_history();
+
+    while (1) {
+        input = readline(FT_PROMPT);
+        
+        // Ctrl+D for exit
+        if (!input) break;
+        
+        if (*input) {
+            add_history(input);
+            printf("cmd: %s\n", input);
+            // send input to lexer
+            // get tokenzation array
+            // check if lexer has everthing done well
+            // send tokenzation array to parser
+            // check if parser do it's work with no error
+            // get AST and check everthing has done well for parser with no error
+            // send AST to exector
+            // executor return a status put it in $? variable
+            // done
+        }
+        free(input);
+    }
+    return 0;
+}
