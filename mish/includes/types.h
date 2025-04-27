32769fd10cd6178212fb42713980ae487f976d4f
ilorez <zobirnajdaoui@gmail.com>
2025-04-27 17:07:52 +0100

    update files

--- mish/includes/types.h
+++ mish/includes/types.h
@@ -6,24 +6,33 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 12:18:55 by znajdaou          #+#    #+#             */
-/*   Updated: 2025/04/26 15:54:58 by znajdaou         ###   ########.fr       */
+/*   Updated: 2025/04/27 17:06:59 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TYPES_H
 #define TYPES_H
-#define FT_PROMPT "minishell> "
+#define FT_NAME "minishell> "
 
-// relations between commands
-typedef enum s_rel {
-  OR,
-  AND,
-  PIPE,
-  REDIR // put redir value in left var
-} t_rel;
+// tokens 
+typedef enum	e_type{
+	ROOT,
+	LPAR,
+	RPAR,
+	AND,
+	OR,
+	PIPE,
+  REDIR,
+	WORD,
+	EOL
+}	t_type;
+
+typedef struct s_hdoc {
+  char *eof;
+} t_hdoc;
 
 typedef struct s_cmd {
-  t_rel type;
+  t_type type;
   void *left;
   void *right;
 } t_cmd;
@@ -33,18 +42,6 @@ typedef struct s_exec
   char **argv;
 } t_exec;
 
-// env
-typedef struct s_env {
-  char *key;
-  char *value;
-} t_env;
-
-
-typedef struct s_prompt {
-  char *prompt;
-  char *path;
-} t_prompt;
-
 typedef struct s_redir {
   char *fpath;
   int fd; // 0 for < and 1 for >
@@ -52,12 +49,21 @@ typedef struct s_redir {
   t_cmd *cmd; // another struct of commands that the rediriction happen on it
 } t_redir;
 
+typedef	struct	s_tokens{
+	t_type	type;
+	void	*value;
+  struct s_tokens *next;
+}	t_tokens;
+
+//t_tokens **tokens;
+
 // data container 
 // Note: this is what will be passsed to execute function
 typedef struct s_data {
-  t_prompt prompt;
-  t_env **vars;
+  char *curr_dir;
+  char *paths;
   t_cmd *ast;
+  bool is_bg;
 } t_data;
 
 #endif
