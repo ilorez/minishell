77458efddc4e49ed6107c32d07281254998f6148
Abdellatif NASSER-ALLAH <nasserallahabdellatif2@gmail.com>
2025-05-08 17:17:48 +0100

    type.h above all the includes

--- mish/includes/container.h
+++ mish/includes/container.h
@@ -11,10 +11,11 @@
 # include <stdlib.h>
 # include <sys/stat.h>
 
+// THIS SHOULD BE ABOVE ALL THE HEADER CUZ IT CONTAINS ALL THE DATA TYPES
+# include "./types.h"
 
 # include "../libft/includes/libft.h"
 # include "./executor.h"
-# include "./types.h"
 # include "./utlis.h"
 # include "./lexer.h"
 
