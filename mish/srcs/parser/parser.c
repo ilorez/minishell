a6552333f178d85244782a07fd6fe959b3671c9f
Abdellatif NASSER-ALLAH <nasserallahabdellatif2@gmail.com>
2025-05-08 10:02:01 +0100

    Auto commit

--- mish/srcs/parser/parser.c
+++ mish/srcs/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: abdnasse <abdnasse@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 13:51:24 by abdnasse          #+#    #+#             */
-/*   Updated: 2025/05/06 16:27:59 by abdnasse         ###   ########.fr       */
+/*   Updated: 2025/05/08 10:01:32 by abdnasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,15 @@
 
 // OR => AND => PIPE => REDIR => WORD => LIST
 
-t_ast	*parse_
+t_ast	*parse_or(t_token **lst)
+{
+	t_ast	**left = parse_and(lst);
+	while (
+}
+
+/*t_ast	*parse_and(t_token **lst)
+t_ast	*parse_pipe(t_token **lst)
+t_ast	*parse_redir(t_token **lst)
+t_ast	*parse_word(t_token **lst)
+t_ast	*parse_list(t_token **lst)
+*/
