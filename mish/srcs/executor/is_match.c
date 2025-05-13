c3ee57464b81de98caeb2d1a4e77f6bfb1785b21
Abdellatif NASSER-ALLAH <nasserallahabdellatif2@gmail.com>
2025-05-13 16:50:12 +0100

    is match

--- mish/srcs/executor/is_match.c
+++ mish/srcs/executor/is_match.c
@@ -1,47 +1,52 @@
 #include "conatainer.h"
 
-//================================== is match ===========
-static void	_skip_till(char *ptr, int *i, char stop);
-static void	_skip_while(char *ptr, int *i, char *match);
 
+static int	find_word(char *name, int *i, char *word)
+{
+	int	j;
+	if (!*word) 
+		return (1);
+
+	while (name[*i])
+	{
+		j = 0;
+		while(word[j] && name[*i + j] == word[j])
+			j++;
+		if (!*(word + j))
+		{
+			*i = *i + j;
+			return 1;
+		}
+		(*i)++;
+	}
+	return 0; // false
+}
 
 int _is_match(t_arr *wild, char *name)
 {
 	int i;
 	int j;
 	char **parts;
-
-	parts = (char **)wild->content;
+	
+	**parts = (char **)wild->content;
 	i = 0;
 	j = 0;
-	if (wild->index == 1 && parts[j][0] == '\0')
+	if (wild->index ==  1 && parts[0][0] == 0)
 		return 0;
-	while (name[i])
+	if (parts[0][0] == 0)
 	{
-		if (parts[j][0] == '\0' && j < wild->index)
-			_skip_till(name, &i, parts[(j++) + 1][0]);
-		if (name[i] == '\0' || name[i] != parts[j][0])
-			return (1); // there is no match 
-		_skip_while(name, &i, parts[j++]);
+		if (!find_word(name, &i, parts[++j]))
+			return 1;  
 	}
-	return (0);
+	else if (strncmp(name, parts[0], strlen(parts[0])))
+		return 1; 
+	while (++j < wild->index)
+		if (!find_word(name, &i, parts[j]))
+			return 1; 
+	i = strlen(name) - strlen(parts[j - 1]);
+	if (parts[j-1][0] == 0)
+		return 0;	
+	else if (name[i] != 0 && !find_word(name, &i, parts[j - 1]))
+		return 1;  
+	return 0;
 }
-
-static void	_skip_till(char *ptr, int *i, char stop)
-{
-	if (!ptr)
-		return ;
-	while (ptr[*i] && ptr[*i] != stop)
-		(*i)++;
-}
-
-static void	_skip_while(char *ptr, int *i, char *match)
-{
-	if (!ptr)
-		return ;
-	while (ptr[*i] && ptr[*i] == *match++)
-		(*i)++;
-	
-}
-//=======================================================
-
