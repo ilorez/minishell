92ab25a2614d2c9aca72e114f2d24fb95dee6e16
Abdellatif NASSER-ALLAH <nasserallahabdellatif2@gmail.com>
2025-05-14 20:02:04 +0100

    working on the redir function

--- mish/srcs/parser/parser.c
+++ mish/srcs/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: abdnasse <abdnasse@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 21:12:46 by abdnasse          #+#    #+#             */
-/*   Updated: 2025/05/14 16:41:07 by abdnasse         ###   ########.fr       */
+/*   Updated: 2025/05/14 20:01:37 by abdnasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,9 +63,9 @@ t_ast	*parse_redir(t_token **lst)
 	t_ast	*right;
 
 	left = parse_word(lst);
-	while (match(lst, T_REDIR))
+	while (match(lst, T_LESS) || match(lst, T_LLESS) ||match(lst, T_GREAT) ||match(lst, T_GGREAT))
 	{
-		next_token(lst);
+		(*lst)->type = T_REDIR;
 		right = parse_word(lst);
 		left = new_node(T_REDIR, NULL, right, left);
 	}
@@ -76,9 +76,14 @@ t_ast	*parse_word(t_token **lst)
 {
 	t_arr	*exec;
 	char	*arg;
+	t_redir	*redir;
 
 	if (match(lst, T_LPAR))
 		return (parse_list(lst));
+	if (match(lst, T_REDIR))
+	{
+		
+	}
 	exec = arr_new();
 	while (match(lst, T_WORD))
 	{
